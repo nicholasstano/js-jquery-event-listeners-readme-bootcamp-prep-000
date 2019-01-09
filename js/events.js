060ae8995e64dde53826000f398a2d34182b97ae
Nicholas Stano <nicholasstano@gmail.com>
2019-01-09 00:30:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,32 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+    
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('g was pressed')
+    }
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
